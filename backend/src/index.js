e971c1cdd71c383bf23e2eeac258e6f205b5a167
andradeJVF <joao.felixandrade1201@gmail.com>
2022-07-25 17:09:02 -0300

    Criando transporter com NodeMailer para envio de e-mails

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-require( './constrollers/authController')(app);
-require( './constrollers/projectController')(app);
+require( './app/constrollers/index')(app);
 
 app.listen(3000);
